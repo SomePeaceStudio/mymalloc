cbaff4854041ad0aef628096bcc971eaebdf2a91
Andis Ozoliņš <Andis Ozoli>
2016-11-24 00:11:30 +0200

    Testēju vai varu veikt izmaiņas

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(int argc, char const *argv[])
 {
 	int x = 0;
 	printf("%s\n", "TODO: Make malloc");
-	printf("%s\n", "keep up the good work");
+	printf("%s\n", "keep up the good work :)");
 	return 0;
 }
