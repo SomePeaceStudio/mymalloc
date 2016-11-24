66734cc615b00c8c6c3bfa2ded2fa5a55887dcd7
SomePeaceStudio <innuss22@gmail.com>
2016-11-24 19:17:42 +0200

    added linked-list structure for storing chunks,requests. Bestfit algorithm.

--- main.c
+++ main.c
@@ -1,13 +1,37 @@
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
+
+// -------------------- Globals and structures ------------------- //
+
+typedef struct chunk
+{
+	size_t size;
+	struct chunk* next;
+} chunk_t;
+
+// --------------------- Function prototypes --------------------- //
 
 void printHelp();
 void fileOpenError(char* filename);
+chunk_t *createChunk(size_t size);
+void addChunkEnd(chunk_t **start, chunk_t **end, chunk_t *newNode);
+void printChunkList(chunk_t *start);
+
+void bestFit(chunk_t *chunkListHead, chunk_t *requestListHead);
+
 
 int main(int argc, char *argv[]){
-
+	
 	char* chunksFilename;
 	char* sizesFilename;
+	chunk_t* chunkListHead = 0;
+	chunk_t* chunkListEnd = 0;
+
+	chunk_t* requestListHead = 0;
+	chunk_t* requestListEnd = 0;
+
+	int sizeBuff; // Buffer for reading sizes and chunks form sizesFile
 
 	if(argc != 5){
 		printHelp();
@@ -29,8 +53,6 @@ int main(int argc, char *argv[]){
 		return 1;
 		
 	}
-	printf("%s\n", chunksFilename);
-	printf("%s\n", sizesFilename);
 	// Open files
 	FILE* chunksFile;
 	FILE* sizesFile;
@@ -42,28 +64,106 @@ int main(int argc, char *argv[]){
 		fileOpenError(sizesFilename);
 		return 1;
 	}
-	// Read in all chunks
-	printf("%s\n", "CHUNKS:");
-	int chunkBuff; // Buffer for reading chunks form chunksFile
-	while(fscanf(chunksFile, "%d", &chunkBuff) > 0) {
-        printf("%d\n", chunkBuff);
+	// Read chunk list
+	while(fscanf(chunksFile, "%d", &sizeBuff) > 0) {
+		addChunkEnd(&chunkListHead,&chunkListEnd,createChunk(sizeBuff));
     }
-	printf("%s\n", "SIZES:");
-    int sizeBuff; // Buffer for reading sizes form sizesFile
+    // Read request list
 	while(fscanf(sizesFile, "%d", &sizeBuff) > 0) {
-        printf("%d\n", sizeBuff);
+		addChunkEnd(&requestListHead,&requestListEnd,createChunk(sizeBuff));
     }
-    // TODO: Test allocation methods
+
+    printf("%s\n", "CHUNKS:");
+    printChunkList(chunkListHead);
+
+    // TODO: Test all allocation methods
+    bestFit(chunkListHead,requestListHead);
+
+    printf("%s\n", "CHUNKS AFTER BEST FIT:");
+    printChunkList(chunkListHead);
+    
 
 	return 0;
 }
 
+// --------------------------------------------------------------- //
+
+void bestFit(chunk_t *chunkListHead, chunk_t *requestListHead){
+	chunk_t *bestFit = 0;
+	// Current chunk
+	chunk_t *currChunk;
+	// Current request 
+	chunk_t *currRequest;
+
+	printChunkList(currRequest);
+	for(currRequest = requestListHead; currRequest != 0; currRequest = currRequest->next){
+		// Find best fit
+		for(currChunk = chunkListHead; currChunk != 0; currChunk = currChunk->next){
+			if(currChunk->size >= currRequest->size){
+				if( bestFit == 0 ){
+					bestFit = currChunk;
+				}else if( currChunk->size - currRequest->size < bestFit->size - currRequest->size ){
+					bestFit = currChunk;
+				}
+			}
+    	}
+    	if(bestFit == 0){
+				printf("%s: Failed to allocate %3d bytes of memory.\n", __func__, (int)currRequest -> size);
+		}else{
+			//printf("Best fit: %d For: %d\n", (int)bestFit->size, (int)currRequest->size);
+			bestFit->size -= currRequest->size;
+			bestFit = 0;
+		}
+    }
+}
+
+
+// --------------------------------------------------------------- //
 
 //TODO: Write better printHelp();
 void printHelp(){
 	printf("Wrong input.\nExpected:\n./file -c chunks.txt -s sizes.txt\n");
 };
 
+// --------------------------------------------------------------- //
+
 void fileOpenError(char* filename){
 	printf("Could not open file: %s\n",filename);
 };
+
+// --------------------------------------------------------------- //
+
+chunk_t *createChunk(size_t size){
+    chunk_t *newNode;
+    newNode = (chunk_t*)malloc(sizeof(chunk_t));
+    // If did not allocate memory
+    if( newNode == NULL ){
+        printf("%s\n", "Error: Could not allocate memory");
+        exit(1);
+    }
+    newNode->size = size;
+    newNode->next = 0;
+    return newNode;
+};
+
+// --------------------------------------------------------------- //
+
+void addChunkEnd(chunk_t **start, chunk_t **end, chunk_t *newNode){
+    // if List is empty
+    if(*start == 0){
+        *start = *end = newNode;
+    }
+    // if List not empty
+    else{
+        (*end)->next = newNode;
+        *end = (**end).next;
+    }
+}
+
+// --------------------------------------------------------------- //
+
+void printChunkList(chunk_t *start){
+    for(chunk_t *current = start; current != 0; current = current->next){
+        printf("Size: %3d \n", (int)current->size);
+    }
+}
